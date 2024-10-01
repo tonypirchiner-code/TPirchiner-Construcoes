2ee49df98d96373723ac1562ac0a1e2ac6d29a50
tonypirchiner-code <tpirchinerprogramacao@gmail.com>
2024-10-01 10:33:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function readText() {
+        const utterance = new SpeechSynthesisUtterance("This is the text that will be read aloud.");
+        speechSynthesis.speak(utterance);
+    }
+    function readText() {
+        const utterance = new SpeechSynthesisUtterance("This is the text that will be read aloud.");
+        speechSynthesis.speak(utterance);
+    }
+        
